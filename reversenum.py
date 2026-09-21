4ae760436a71e6d386252ea6e122e9f40cf8c2e6
sohampakhare <sohampakhare71@gmail.com>
2026-09-21 10:56:44 +0530

    version 9..8

---
+++ reversenum.py
@@ -0,0 +1,6 @@
+n=int(input("enter a number"))
+
+while n>0:
+    l=n%10
+    print(l,end="")
+    n=n//10
--- totalnumbers.py
+++ totalnumbers.py
@@ -1,1 +1,8 @@
-print("hello")
+n=int(input("enter a number : "))
+count=0
+
+while n>0:
+    n=n//10
+    count +=1
+
+print(count)
